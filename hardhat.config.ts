057baae949a8f7c05d2bb78801c9163b7b5f6416
Matthew Mulford <mhmulford0@gmail.com>
2021-10-01 14:42:45 -0500

    update

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,23 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
 import { HardhatUserConfig } from "hardhat/types";
 import "@nomiclabs/hardhat-waffle";
+
+const { ACCOUNT_ADDRESS } = process.env;
+
 const config: HardhatUserConfig = {
   solidity: {
     compilers: [{ version: "0.8.4", settings: {} }],
   },
+  networks: {
+    hardhat: {
+      chainId: 1337
+    },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/e5aa55e375464ff6b7bd71d4b31589d5",
+      accounts: [`0x${ACCOUNT_ADDRESS}`]
+    }
+  }
 };
 export default config;
--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,116 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+
+import Greeter from "../artifacts/contracts/Greeter.sol/Greeter.json";
+
+const greeterAddress = "0xe1dA78c984E567f934D54d7fE5f335352F494370";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [greeting, setGreetingValue] = useState("");
+  const [initialGreeting, setInitialGreeting] = useState("")
+  const [address, setAddress] = useState("");
+  const [pending, setPending] = useState(false);
+  const [txInfo, setTxInfo] = useState({
+    transactionHash: null,
+  });
+
+  useEffect(() => {
+    fetchGreeting();
+  }, []);
+
+  const requestAccount = async () => {
+    try {
+      setAddress(
+        await window.ethereum.request({ method: "eth_requestAccounts" })
+      );
+    } catch (error) {
+      console.log(error)
+    }
+
+  };
+
+  const fetchGreeting = async () => {
+    if (typeof window.ethereum === undefined) return;
+
+    // provider for ethers is metamask
+    
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    // contract to interact with, requires: contract address, ABI, and provider
+    const contract = new ethers.Contract(greeterAddress, Greeter.abi, provider);
+
+    try {
+      const data = await contract.greet();
+      setGreetingValue(data)
+      setInitialGreeting(data)
+      console.log("Contract Returned: ", data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const setGreeting = async () => {
+    if (!greeting) return;
+    if (typeof window.ethereum === undefined) return;
+    try {
+      await requestAccount();
+      setPending(true);
+      setTxInfo({transactionHash: null})
+       // get metamask wallet address
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(greeting);
+      setTxInfo(await transaction.wait());
+      fetchGreeting();
+      setPending(false);
+    } catch (error) {
+      console.log(error)
+    }
+   
+
+  };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
+    <div
+      className="App"
+      style={{
+        display: "flex",
+        flexFlow: "column wrap",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "5rem"
+      }}
+    >
+      <header className="App-header"><h2 style={{marginBottom: "40px"}}>BIG DApp</h2></header>
+      <h2>Current Greeting: {initialGreeting} </h2>
+      <p>
+     
+      </p>
+      <p>
+        <input
+          type="text"
+          name="greeting"
+          onChange={(e) => setGreetingValue(e.target.value)}
+          placeholder="Enter Greeting"
+        />
+        <button onClick={setGreeting}>Set Greeting</button>
+        
+      </p>
+      <p>Your Address: {address} </p>
+      {pending ? <p>Transaction Pending...</p> : ""}
+      {txInfo.transactionHash !== null ? (
         <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+          View your transaction here:{" "}
+          <a href={`https://ropsten.etherscan.io/tx/${txInfo.transactionHash}`}>
+            Transaction Info
           </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
         </p>
-      </header>
+      ) : (
+        ""
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
